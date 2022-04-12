b2289d986e6e517aec5674969cb04d8157a4b67c
bachmidiqbal <iqbal.bachmid@gmail.com>
2022-04-12 14:53:42 +0700

    add bank functionalities

---
+++ bankcore/bank_test.go
@@ -0,0 +1,130 @@
+package bankcore
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAccount(t *testing.T) {
+	account := Account{
+		Customer: Customer{
+			Name:    "John",
+			Address: "Los Angeles, California",
+			Phone:   "(213) 555 0147",
+		},
+		Number:  1001,
+		Balance: 0,
+	}
+
+	if account.Name == "" {
+		t.Error("can't create an Account object")
+	}
+}
+
+func TestDeposit(t *testing.T) {
+	account := Account{
+		Customer: Customer{
+			Name:    "John",
+			Address: "Los Angeles, California",
+			Phone:   "(213) 555 0147",
+		},
+		Number:  1001,
+		Balance: 0,
+	}
+
+	account.Deposit(10)
+
+	if account.Balance != 10 {
+		t.Error("balance is not being updated after a deposit")
+	}
+}
+
+func TestDepositInvalid(t *testing.T) {
+	account := Account{
+		Customer: Customer{
+			Name:    "John",
+			Address: "Los Angeles, California",
+			Phone:   "(213) 555 0147",
+		},
+		Number:  1001,
+		Balance: 0,
+	}
+
+	if err := account.Deposit(-10); err == nil {
+		t.Error("only positive numbers should be allowed to deposit")
+	}
+}
+
+func TestWithdraw(t *testing.T) {
+	account := Account{
+		Customer: Customer{
+			Name:    "John",
+			Address: "Los Angeles, California",
+			Phone:   "(213) 555 0147",
+		},
+		Number:  1001,
+		Balance: 0,
+	}
+
+	account.Deposit(10)
+	account.Withdraw(10)
+
+	if account.Balance != 0 {
+		t.Error("balance is not being updated after withdraw")
+	}
+}
+
+func TestWithdrawWithNegativeAmount(t *testing.T) {
+	account := Account{
+		Customer: Customer{
+			Name:    "John",
+			Address: "Los Angeles, California",
+			Phone:   "(213) 555 0147",
+		},
+		Number:  1001,
+		Balance: 0,
+	}
+
+	account.Deposit(10)
+	err := account.Withdraw(-1)
+
+	assert.Equal(t, "the amount to withdraw should be greater than zero", err.Error())
+}
+
+func TestWithdrawWithAmountGreaterThanBalance(t *testing.T) {
+	account := Account{
+		Customer: Customer{
+			Name:    "John",
+			Address: "Los Angeles, California",
+			Phone:   "(213) 555 0147",
+		},
+		Number:  1001,
+		Balance: 0,
+	}
+
+	account.Deposit(10)
+	err := account.Withdraw(11)
+
+	assert.Equal(t, "the amount to withdraw should be less or equal current balance", err.Error())
+}
+
+func TestStatement(t *testing.T) {
+	account := Account{
+		Customer: Customer{
+			Name:    "John",
+			Address: "Los Angeles, California",
+			Phone:   "(213) 555 0147",
+		},
+		Number:  1001,
+		Balance: 0,
+	}
+
+	account.Deposit(10)
+	account.Withdraw(5)
+
+	statement := account.Statement()
+
+	if statement != "1001 - John - 5" {
+		t.Error("statement doesn't have the proper format")
+	}
+}
